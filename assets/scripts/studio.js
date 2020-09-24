f4507ae782905922b6059c1e9dbc09c0e09a1786
lockdowncommute <homeoffice@roxyzeiher.com>
2020-09-24 16:42:31 +0200

    Audio Test

--- assets/scripts/studio.js
+++ assets/scripts/studio.js
@@ -73,23 +73,6 @@ function init() {
   textureEquirec2.encoding = THREE.sRGBEncoding;
 
 
-  // audio
-  // create an AudioListener and add it to the camera
-var listener = new THREE.AudioListener();
-camera.add( listener );
-
-// create a global audio source
-var sound = new THREE.Audio( listener );
-
-// load a sound and set it as the Audio object's buffer
-var audioLoader = new THREE.AudioLoader();
-audioLoader.load( 'assets/audio/Bex.mp3', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 3 );
-	sound.play();
-});
-
 
 
   //create Equirectangular shader
