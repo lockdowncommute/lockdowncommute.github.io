03707dd1f0d2a59309bd877490e753c45d39759e
lockdowncommute <homeoffice@roxyzeiher.com>
2020-09-24 13:28:07 +0200

    Sort + Customise Code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,45 +7,23 @@ $(document).ready(function(){
   checkWidth();
   $(window).resize(checkWidth);
 
-  $('.tracklist-button').click(function(){
-    $(this).parent().prev('.box-li').find('.flip-box').toggleClass('active');
-  })
-
-  $('.mix-scaler').click(function(){
-    var button = $(this).find('.mix-opener');
-
-    if ($(this).siblings('.mix-content-wrapper').length) {
-      if (!$(this).hasClass('active')) {
-        $(this).siblings('.mix-content-wrapper').slideToggle(400);
-        $(button).html('&uarr;');
-        $(this).addClass('active');
-       } else {
-         $(button).html('&darr;');
-         $(this).removeClass('active');
-         $(this).siblings('.mix-content-wrapper').slideToggle(400);
-       };
-    } 
-  });
 
   if (windowSize > 950) {
-    $('.about p, .map-wrapper p').mouseenter(function() {
+    $('.about p').mouseenter(function() {
        $( this ).stop().animate({letterSpacing: '5px' }, 2000);
     });
-    $('.about p, .map-wrapper p').mouseleave(function() {
+    $('.about p').mouseleave(function() {
        $( this ).stop().animate({letterSpacing: '0px' }, 200);
     });
-    $('.square-wrapper').hover(function(){
-      $(this).children('.artwork').fadeToggle(400);
-    })
   }
 
-  $('#enterButton').click(function(){
+  $('#infoButton').click(function(){
     $('html, body').animate({scrollTop: ($('.about').offset().top)},1000);
   })
 
   $('#laClose, #three-wrapper').click(function(){
-    $('#lookAround').fadeOut(200);
-    $('#enterButton').fadeIn(200);
+    $('#Arrows').fadeOut(200);
+    $('#infoButton').fadeIn(200);
   })
 
 
